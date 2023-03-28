23005cb881d5a90ffc2eea6f530400e852403f31
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2023-03-28 15:52:49 +0700

    feat: setting verification input form

--- src/components/mitra/settings/Verification.jsx
+++ src/components/mitra/settings/Verification.jsx
@@ -1,15 +1,41 @@
 import "./style.scss";
 import { useState } from "react";
 import TopbarSettings from "./TopbarSettings";
+import Validation from "./validations";
 
 const VerificationMitra = () => {
   const [image, setImage] = useState("");
+  const [errors, setErrors] = useState({});
 
   const imageUpload = (e) => {
     const data = e.target.files[0];
     setImage(data);
   };
   const [showPromo, setShowPromo] = useState(false);
+
+  const initialValue = {
+    no_npwp: "",
+    nama_usaha: "",
+    image_ktp: "",
+    output_value: "",
+  };
+
+  const [formData, setFormData] = useState(initialValue);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // const handleValidation = (e) => {
+  //   e.preventDefault();
+
+  //   setErrors(Validation(formData));
+  // };
+
+  console.log(errors);
+
+  console.table(formData);
 
   return (
     <TopbarSettings>
@@ -18,46 +44,48 @@ const VerificationMitra = () => {
           <input
             className=""
             name="output_value"
-            // onChange={handleChange}
+            // onChange={(e) => handleChange(e)}
             type="radio"
-            value="normal"
+            value="pemerintah"
             onClick={() => setShowPromo(true)}
           />
           <label className="pr-10">Pemerintah</label>
           <input
             className=""
             name="output_value"
-            // onChange={handleChange}
+            // onChange={(e) => handleChange(e)}
             type="radio"
-            value="promo"
+            value="nonpemerintah"
             onClick={() => setShowPromo(false)}
           />
           <label>non Pemerintah</label>
         </div>
         {showPromo ? (
-          <section className="validasiForm flex">
+          <form className="validasiForm flex" onSubmit={handleValidation}>
             <div className="formInputContent flex">
               <label className="titleForm">NPWP</label>
               <input
-                name="no_npwp"
+                name="npwp"
                 className="radius-2"
                 type="number"
                 placeholder="Masukan nomor npwp..."
-                // onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChange(e)}
               />
+              {errors.npwp && <p>{errors.npwp}</p>}
             </div>
             <div className="formInputContent flex">
               <label className="titleForm">Nama Usaha</label>
               <p className="sub">*nama usaha yang terdaftar dalam npwp</p>
               <input
-                name="email_mitra"
+                name="nama_usaha"
                 className="radius-2"
-                type="email"
+                type="text"
                 placeholder="Masukan nama usaha..."
-                // onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChange(e)}
               />
             </div>
-          </section>
+            <button>submit</button>
+          </form>
         ) : null}
         <div className="uploadKtp flex">
           <div className="topUploadKtp">
---
+++ src/utils/validation.js
@@ -0,0 +1,9 @@
+// export default function Validation({ formData }) {
+//   const errors = {};
+
+//   if (formData.no_npwp === "4") {
+//     errors.no_npwp = "Harap Mengisi nomor NPWP dalam verifikasi akun";
+//   }
+
+//   return errors;
+// }
